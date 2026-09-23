65a0a713b5ec9489cbe49af290501a72e894d34e
praveen-73 <praveenrsp25@gmail.com>
2026-09-23 23:30:09 +0530

    adfas

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+        
+#         'NAME': BASE_DIR / 'db.sqlite3',
+
+
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Droidal@1207',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
